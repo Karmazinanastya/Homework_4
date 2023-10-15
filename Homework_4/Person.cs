93dd56dbec4f1360876e987433ca6fc54b937296
Karmazinanastya <karmazina133@gmail.com>
2023-10-15 22:27:34 +0300

    Add project files.

---
+++ Homework_4/Person.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Homework_4
+{
+    internal class Person
+    {
+        private string _name;
+        private DateTime _birthYear;
+
+        public string Name 
+        {
+            get { return _name; }
+        }
+
+        public DateTime BirthYear 
+        {
+            get { return _birthYear; }
+        }
+
+        public Person()
+        {
+            _name = "Jon";
+            _birthYear = new DateTime();
+        }
+
+        public Person(string name, DateTime birthYear) 
+        {
+            _name = name;
+            _birthYear = birthYear;
+        }
+
+        public int Age()
+        {
+            DateTime now = DateTime.Now;
+            int age = now.Year - _birthYear.Year;
+
+            if (now < _birthYear.AddYears(age)) age--;
+            return age;
+        }
+
+        public void Input()
+        {
+            Console.Write("Введіть ім'я: ");
+            _name = Console.ReadLine();
+
+            Console.Write("Введіть рік народження: ");
+            if (DateTime.TryParse(Console.ReadLine(), out DateTime birthYear))
+            {
+                _birthYear = birthYear;
+            }
+            else
+            {
+                Console.WriteLine("Невiрний формат року народження.");
+            }
+        }
+
+        public void ChangeName(string NewName) 
+        {
+            _name = NewName;
+
+        }
+        public override string ToString()
+        {
+            return $"Iм'я: {_name}, Вiк: {Age()}";
+        }
+
+        public void Output()
+        {
+            Console.WriteLine(ToString());
+        }
+
+        public static bool operator ==(Person person1, Person person2)
+        {
+            return person1.Name == person2.Name;
+        }
+
+        public static bool operator !=(Person person1, Person person2)
+        {
+            return !(person1 == person2);
+        }
+    }
+}
+
---
+++ Homework_4/Program.cs
@@ -0,0 +1,88 @@
+﻿using Homework_4;
+
+class Program
+{
+    static void Main()
+    {
+        //List<Person> people = new List<Person>();
+
+        //for (int i = 0; i < 6; i++)
+        //{
+        //    Person person = new Person();
+        //    person.Input();
+        //    people.Add(person);
+        //}
+
+        List<Person> people = new List<Person>()
+        {
+            new Person("Nastya", new DateTime(2005, 4, 3)),
+            new Person("Liza", new DateTime(2004, 5, 6)),
+            new Person("Vova", new DateTime(2009, 9, 2)),
+            new Person("Nazar", new DateTime(2005, 7, 8)),
+            new Person("Nika", new DateTime(2008, 3, 6)),
+            new Person("Jeni", new DateTime(2012, 2, 1)),
+        };
+
+        foreach (Person person in people)
+        {
+            Console.WriteLine(person.ToString());
+        }
+        Console.WriteLine("\nЗмiнити iм'я осiб, вiк яких менше 16 рокiв, на 'Дуже молодий':");
+
+        foreach (var person in people)
+        {
+            if (person.Age() < 16)
+            {
+                person.ChangeName("Дуже молодий");
+            }
+        }
+
+        Console.WriteLine("\nIнформацiя про всiх людей пiсля змiни iмен:");
+
+        foreach (var person in people)
+        {
+            person.Output();
+        }
+
+        Console.WriteLine("\nОсоби з однаковими iменами:");
+        for (int i = 0; i < people.Count; i++)
+        {
+            for (int j = i + 1; j < people.Count; j++)
+            {
+                if (people[i].Name == people[j].Name)
+                {
+                    Console.WriteLine();
+                    Console.WriteLine($"Особа {i + 1}: {people[i].Name}, Вiк: {people[i].Age()}");
+                    Console.WriteLine($"Особа {j + 1}: {people[j].Name}, Вiк: {people[j].Age()}");
+                }   // перевіряє усі пари повторюваних осіб
+            }
+        }
+
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
